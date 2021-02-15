6068eaa896e1ceaee836d0c142b06685127f25e5
Brais Cidras <braisomary@gmail.com>
2021-02-15 19:40:12 +0200

    Subscribe method (Handling errors)

--- src/main/java/com/merlobranco/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/merlobranco/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.merlobranco.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,8 @@ import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
@@ -15,10 +19,17 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Andres", "Pedro", "Diego", "Juan")
-				.doOnNext(System.out::println);
+		Flux<String> nombres = Flux.just("Andres", "Pedro", "", "Diego", "Juan")
+				.doOnNext(e -> {
+					if (e.isEmpty()) {
+						throw new RuntimeException("Nombres no pueden ser vacíos");
+					}
+					System.out.println(e);
+				});
 		
-		nombres.subscribe();
+		// Subscribing the observer with a task
+		// And handling an error
+		nombres.subscribe(e -> log.info(e), error -> log.error(error.getMessage()));
 		
 	}
 
